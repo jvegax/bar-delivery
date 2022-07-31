74a456feed03192963899b952c58ec0d6ebfc2de
jvegax <toledo.utrera@hotmail.com>
2022-07-31 18:31:52 +0200

    fix

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -3,20 +3,19 @@ import { PrismaClient } from "@prisma/client";
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
 
-  // if (req.method === "POST") {
-  //   const nuevaOrden = await prisma.orden.create({
-  //     data: {
-  //       orden: req.body.pedido,
-  //       fecha: req.body.fecha,
-  //       nombre: req.body.nombre,
-  //       total: req.body.total,
-  //     },
-  //   });
+  if (req.method === "POST") {
+    const nuevaOrden = await prisma.orden.create({
+      data: {
+        orden: req.body.pedido,
+        fecha: req.body.fecha,
+        nombre: req.body.nombre,
+        total: req.body.total,
+      },
+    });
 
-  //   res.json(nuevaOrden);
-  // }
-  // if (req.method === "GET") {
+    res.json(nuevaOrden);
+  }else{
     const orders = await prisma.orden.findMany();
     res.status(200).json(orders);
-  // }
+  }
 }
